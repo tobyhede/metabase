d605429ff0868461b4ce944b274f298eeed81691
Toby Hede <tobyhede@info-architects.net>
2013-09-23 14:29:23 -0700

    java 7

--- recipes/cassandra.rb
+++ recipes/cassandra.rb
@@ -5,6 +5,9 @@
 # Copyright 2013, Toby Hede
 #
 
-node.set[:cassandra][:version] = "2.0"
+node.set["java"]["install_flavor"] = "openjdk"
+node.set["java"]["jdk_version"] = "7"
+
+node.set["cassandra"]["version"] = "2.0"
 
 include_recipe "cassandra::datastax"
