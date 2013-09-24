e5bcb1004331ebfc17d8d96a8ead435000871e7f
Toby Hede <tobyhede@info-architects.net>
2013-09-23 19:58:51 -0700

    try oracle

--- Berksfile
+++ Berksfile
@@ -4,7 +4,7 @@ metadata
 
 cookbook "postgresql", :git => "git@github.com:tobyhede/postgresql.git"
 cookbook "cassandra", :git => "git@github.com:tobyhede/cassandra-chef-cookbook.git"
+cookbook "java", :git => "git@github.com:opscode-cookbooks/java.git"
 
 #cookbook "postgresql", :git => "git@github.com:opscode-cookbooks/postgresql.git"
 #cookbook "docker", :git => "git@github.com:bflad/chef-docker.git"
-
--- recipes/cassandra.rb
+++ recipes/cassandra.rb
@@ -5,10 +5,12 @@
 # Copyright 2013, Toby Hede
 #
 
-node.set["java"]["install_flavor"] = "openjdk"
+node.set["java"]["install_flavor"] = "oracle"
 node.set["java"]["jdk_version"] = "7"
+node.set["java"]["oracle"]["accept_oracle_download_terms"] = true
 
 node.set["cassandra"]["version"] = "2.0"
 node.set["cassandra"]["package_version"] = "dsc20"
 
+include_recipe "java::default"
 include_recipe "cassandra::datastax"
